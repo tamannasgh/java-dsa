69876c359313238f94d5d0d995fc7bbcd53a6ebb
Tamanna Sharma <ts.harma2882006@gmail.com>
2022-07-07 15:09:15 +0530

    binary search in a different way (maximizing or minimizing)

--- 1Practice/searching/code/src/GuessNumber.java
+++ 1Practice/searching/code/src/GuessNumber.java
@@ -1,2 +1,39 @@
 public class GuessNumber {
+    public static void main(String[] args) {
+        System.out.println(guessNumber(10));
+    }
+
+    static int guessNumber(int n){
+        int s = 1;
+        int e = n;
+
+        while(s <= e){
+            int m = s + (e-s)/2;
+
+            int result = guess(m);
+
+            if(result == 0){
+                return m;
+            }
+            if(result == 1){
+                s = m + 1;
+            } else{
+                e = m -1;
+            }
+        }
+
+        return -1;
+    }
+
+    static int guess(int guessed){
+        int num = 4;
+        if (guessed == num) {
+            return 0;
+        }
+        if(guessed < num){
+            return 1;
+        } return -1;
+    }
+
+
 }
---
+++ 1Practice/searching/code/src/ShipCapacity.java
@@ -0,0 +1,45 @@
+public class ShipCapacity {
+    public static void main(String[] args) {
+        int[] arr = {3,2,2,4,1,4};
+        int days = 3;
+        System.out.println(shipCap(arr, days));
+    }
+
+    static int shipCap(int[] arr, int days){
+        int s = arr[0];
+        int e = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            s = Math.max(s, arr[i]);
+            e += arr[i];
+        }
+
+        while(s < e){
+            int mid = s + (e-s)/2;
+
+            int daysAccToMid = daysNeeded(arr, mid);
+
+            if(daysAccToMid <= days){
+                e = mid;
+            } else{
+                s = mid +1;
+            }
+        }
+        return s;
+    }
+
+    static int daysNeeded(int[] arr, int shipCapacity){
+        int daysNeed = 1;
+        int weight = 0;
+
+        for (int weightOfPackage : arr) {
+            if (weight + weightOfPackage > shipCapacity) {
+                daysNeed++;
+                weight = weightOfPackage;
+            } else {
+                weight += weightOfPackage;
+            }
+        }
+        return daysNeed;
+    }
+}
